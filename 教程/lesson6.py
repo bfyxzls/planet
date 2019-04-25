6e58a986775904b654c24313eb01822c3f446fac
zhangzl <zhangzl@i-counting.cn>
2019-04-25 15:22:15 +0800

    添加了类的实例

--- 教程/lesson6.py
+++ 教程/lesson6.py
@@ -1,8 +1,22 @@
-class MyClass:
-    name = "zhang.lind"
+class MyClass():
+    def __init__(self,name="",age=0):
+        self.name=name
+        self.age=age
+    def __del__(self):
+        print("MyClass is delete.")
 
     def printf(self):
-        print ('hello world')
+        print ('hello world, %s' %(self.name))
 
-myclass=MyClass()
+    def printf2(self):
+        print ('hello world,' ,self.name)
+
+    def printf3(self):
+        print ('姓名:%s,年龄:%s' %(self.name,self.age))
+
+myclass=MyClass("zzl")
 myclass.printf()
+myclass.printf2()
+
+myclass3 = MyClass("zzl", "35")
+myclass3.printf3()
